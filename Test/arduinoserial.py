2ffb63c4813e6f1202278207f39562f707438069
Thorinn <thorinn_e@msn.com>
2020-06-09 11:06:30 -0600

    Arduino python connection 1st program

---
+++ Test/arduinoserial.py
@@ -0,0 +1,9 @@
+import serial
+
+arduinoSerialData = serial.Serial("com3", 9600)
+
+while (1 == 1):
+    if (arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline()
+        print(myData)
+    
